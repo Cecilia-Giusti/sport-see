afe196f7206ad974932fb26da0c452bd790fe0b3
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-09-07 09:00:35 +0200

    Feat: update macros

--- src/components/Macros.jsx
+++ src/components/Macros.jsx
@@ -4,10 +4,11 @@ import caloriesIcon from "../assets/calories-icon.png";
 import carbsIcon from "../assets/carbs-icon.png";
 import fatIcon from "../assets/fat-icon.png";
 import proteinIcon from "../assets/protein-icon.png";
+import "../style/macros.css";
 
 const Macros = ({ macros }) => {
   return (
-    <div className="macros__content">
+    <aside className="macros__container">
       <Card type="Calories" number={macros.calorieCount} image={caloriesIcon} />
       <Card type="Proteines" number={macros.proteinCount} image={proteinIcon} />
       <Card
@@ -16,7 +17,7 @@ const Macros = ({ macros }) => {
         image={carbsIcon}
       />
       <Card type="Lipides" number={macros.lipidCount} image={fatIcon} />
-    </div>
+    </aside>
   );
 };
 
